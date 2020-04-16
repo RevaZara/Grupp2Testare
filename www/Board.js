613622378c1d51b302cbce31dde647050b318bb0
RevaZara <reva1zara1@hotmail.com>
2020-04-16 22:07:40 +0200

    lagt in metoder och constructor

--- www/Board.js
+++ www/Board.js
@@ -1,4 +1,13 @@
-class Board { }
+class Board { 
+  constructor(game){}
+  async makeMove(column){}
+  winCheck(){}
+  render(){}
+  markWin(combo){}
+  addEventListener(){}
+  removeEventListener(){}
+
+}
 
 // make it possible to test on backend
 if (typeof global !== 'undefined') { global.Board = Board };
