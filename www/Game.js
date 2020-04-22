bfc7b068763ce8c1616d833355421239cae2dd89
RevaZara <reva1zara1@hotmail.com>
2020-04-22 22:47:07 +0200

    koda

--- www/Game.js
+++ www/Game.js
@@ -1,12 +1,43 @@
-class Game { 
-  constructor() { }
-  start() { }
-  tellTurn(player) { }
-  over(won) {}
-  addEventListener(){}
+class Game {
 
+  constructor() {
+    this.addEventListeners();
+    this.start();
+  }
+  start() {
+    this.board = new Board(this);
+  }
+  tellTurn(player) {
+    if (player == 1 || player == 2) {
+      $(".message").innerHTML = player == 1 ? 'Röds tur…' : 'Guls tur…';
+    } else {
+      throw (new Error('player must be 1 or 2'));
+    }
+  }
+  over(won) {
+    if (won == 1 || won == 2 || won == "draw") {
+      if (won === "draw") {
+        $(".message").innerHTML = "Det blev oavgjort!";
+      } else if (won === 1) {
+        $(".message").innerHTML = "Röd vann!";
+      } else {
+        $(".message").innerHTML = "Gul vann!";
+      }
+    } else {
+      throw (new Error('won must be “draw”, 1 or 2'));
+    }
+  }
+  addEventListeners() {
+    $('.message').addEventListener('click', e => {
+      if (e.target.closest('.again')) {
+        this.start();
+      }
+    });
+  }
 }
 
 
 // make it possible to test on backend
-if (typeof global !== 'undefined') { global.Game = Game };
+if (typeof global !== 'undefined') {
+  global.Game = Game
+};
