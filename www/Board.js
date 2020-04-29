f91e8fe3f41258a0ba25ee334ade93a0babd413c
moona <memoona.tahira@gmail.com>
2020-04-29 15:13:22 +0200

    clear baord before rander

--- www/Board.js
+++ www/Board.js
@@ -65,6 +65,7 @@ class Board {
     }
 
     render() {
+        $('.board').innerHTML = '';
         for (let i = 0; i < this.matrix.length; i++) {
             for (let j = 0; j < this.matrix[i].length; j++) {
                 let div1 = document.createElement('div');
