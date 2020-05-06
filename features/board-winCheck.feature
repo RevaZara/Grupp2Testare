0bbd20d9199183ff3c6bb79b067dc6a153982e5c
moona <memoona.tahira@gmail.com>
2020-05-06 11:01:30 +0200

    more wincheck scenarios

---
+++ features/board-winCheck.feature
@@ -0,0 +1,20 @@
+Feature: Play Connect 4
+  As a poor Connect 4 player
+  I want to play the game on my screen with my friends
+  So that I don't need to buy it.
+
+  Scenario: After player have four consecutive slots, player is declared a winner
+    Given   game is in progress
+    When   the player have four consecutive slots
+    Then   it should tell who wins
+
+  Scenario: Its a draw when slots are full
+    Given   game is in progress
+    When    no more slots are available
+    Then    than it should say draw
+
+  Scenario: If game is not done, it must return false
+    Given   game is in progress
+    Then    the method should return the value false
+
+
---
+++ step-definitions/board-winCheck.js
@@ -0,0 +1,73 @@
+// This requires Game, Board, sleep, $ and $$ as globals
+// (do this in each step-definition file in this project)
+require('./_include-all')();
+
+module.exports = function () {
+
+  this.After(() => fixNoSuchWindowError(driver));
+
+  let board;
+
+  //  Scenario: when player have four consecutive slots
+  //     Given   the game is in progress
+  //     When   the player have 4 consecutive slots
+  //     Then   it should tell who wins
+  this.Given(/^game is in progress$/, () => {
+    game = new Game();
+    board = new Board(game);
+  })
+
+  this.When(/^the player have four consecutive slots$/, async function () {
+    await board.makeMove(1);
+    await board.makeMove(2);
+    await board.makeMove(1);
+    await board.makeMove(2);
+    await board.makeMove(1);
+    await board.makeMove(2);
+    await board.makeMove(1);
+
+
+
+  });
+
+  this.Then(/^it should tell who wins$/, function () {
+    let result = board.winCheck();
+    console.log(result);
+    expect(result.winner).to.equal(1);
+  });
+
+  //  Scenario: Its a draw when slots are full
+  //     Given   game is in progress
+  //     When    no more slots are available
+  //     Then    than it should say draw
+  //
+  this.When(/^no more slots are available$/, function () {
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 7; j++) {
+        board.matrix[i][j] = 5
+      }
+    }
+  });
+
+  this.Then(/^than it should say draw$/, function () {
+    let result = board.winCheck();
+    console.log(result);
+    expect(result.winner).to.equal("draw");
+  });
+
+  this.Then(/^the method should return the value false$/, function () {
+    let result = board.winCheck();
+    console.log(result);
+    expect(result).to.be.false;
+  });
+
+
+
+
+
+}
+
+
+
+
+
